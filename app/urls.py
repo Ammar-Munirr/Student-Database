14eb87ff0898d961e2595af9e0f664862b05e831
Able <munirammar0@gmail.com>
2023-06-04 12:01:34 +0500

    adding urls and view function

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.add,name='add')
+    path('',views.add,name='home'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Student
+from .forms import StudentForm
 # Create your views here.
 def add(request):
-    pass
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = StudentForm()
+    data = Student.objects.all()
+    return render(request,'add.html',{'form':form,'students':data})
